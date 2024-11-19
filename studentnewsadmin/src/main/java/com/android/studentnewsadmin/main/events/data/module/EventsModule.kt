8c45ebb4ac84cd12477612a6f87f6ce6e174ee65
Abdullah <abdullah.hameed2087@gmail.com>
2024-11-19 17:19:07 +0500

    Uploading Events UI part Completed in Student News Admin Module in events-feature.

---
+++ studentnewsadmin/src/main/java/com/android/studentnewsadmin/main/events/data/module/EventsModule.kt
@@ -0,0 +1,17 @@
+package com.android.studentnewsadmin.main.events.data.module
+
+import com.android.studentnewsadmin.main.events.data.repository.EventsRepositoryImpl
+import com.android.studentnewsadmin.main.events.domain.repository.EventsRepository
+import com.android.studentnewsadmin.main.events.ui.viewModels.EventsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+
+val eventsModule = module {
+
+    singleOf(::EventsRepositoryImpl) { bind<EventsRepository>()  }
+
+    viewModelOf(::EventsViewModel)
+
+}
