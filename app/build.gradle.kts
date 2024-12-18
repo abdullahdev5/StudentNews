550991712c84417b17583a0c9045bb96a883bbe4
abdullah.hameed2087@gmail.com <abdullah.hameed2087@gmail.com>
2024-12-18 12:47:14 +0500

    Adding Lottie animation in referral_bonus.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,7 @@ dependencies {
     // paging 3
     implementation(libs.androidx.paging.runtime.ktx)
     implementation(libs.androidx.paging.compose)
+
+    // Lottie Animation
+    implementation(libs.airbnb.android.lottie.compose)
 }
